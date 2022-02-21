b8e3d90694568f8b9728b56c6bc3ac6b79cb6146
shirazshabo <shirazautomation@gmail.com>
2022-02-21 12:45:25 +0200

    add severity to Allure report

--- src/test/java/tests/SortTasks.java
+++ src/test/java/tests/SortTasks.java
@@ -3,10 +3,13 @@ package tests;
 import org.testng.annotations.Test;
 
 import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import pageobjects.TasksPage;
 
 public class SortTasks extends BaseTest {
 
+	@Severity(SeverityLevel.NORMAL)
 	@Test(description="sort tasks by priority")
 	@Description("sort all tasks in the chosen list by priority")
 	public void tc01_sortByPriority () throws InterruptedException {
@@ -14,6 +17,7 @@ public class SortTasks extends BaseTest {
 		tasksPage.sortTasksByPriority("shirazNew");	
 	}
 	
+	@Severity(SeverityLevel.NORMAL)
 	@Test(description="sort tasks by due date")
 	@Description("sort all tasks in the chosen list by due date")
 	public void tc02_sortByDueDate() throws InterruptedException {
