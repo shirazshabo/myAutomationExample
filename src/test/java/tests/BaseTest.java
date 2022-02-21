30a3d607e5d18228fd3a801afaa9133281882800
shirazshabo <shirazautomation@gmail.com>
2022-02-21 19:07:28 +0200

    screenshots fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -50,19 +50,19 @@ public class BaseTest {
 		driver.quit();
 	}
 
-	@AfterMethod
-	public void failedTest(ITestResult result) {
-		//check if the test failed
-		if (result.getStatus() == ITestResult.FAILURE ){
-			TakesScreenshot ts = (TakesScreenshot)driver;
-			File srcFile = ts.getScreenshotAs(OutputType.FILE);
-			try {
-				FileUtils.copyFile(srcFile, new File("./ScreenShots/"+result.getName()+".jpg"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			//result.getname() method will give you current test case name. 
-			//./ScreenShots/ tell you that, in your current directory, create folder ScreenShots. dot represents current directory
-		}
-	}
+//	@AfterMethod
+//	public void failedTest(ITestResult result) {
+//		//check if the test failed
+//		if (result.getStatus() == ITestResult.FAILURE ){
+//			TakesScreenshot ts = (TakesScreenshot)driver;
+//			File srcFile = ts.getScreenshotAs(OutputType.FILE);
+//			try {
+//				FileUtils.copyFile(srcFile, new File("./ScreenShots/"+result.getName()+".jpg"));
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//			//result.getname() method will give you current test case name. 
+//			//./ScreenShots/ tell you that, in your current directory, create folder ScreenShots. dot represents current directory
+//		}
+//	}
 }
