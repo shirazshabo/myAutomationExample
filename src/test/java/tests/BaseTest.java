667f823c472f84657aa895a87c5e76b046cb3584
shirazshabo <shirazautomation@gmail.com>
2022-02-21 18:12:10 +0200

    fix tests

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,7 +8,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
